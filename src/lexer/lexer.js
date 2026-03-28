1db735d883de76b98b0872c4dd5851c1cedf96fe
Soham Deep Chauhan <akashdeepchauhan9@gmail.com>
2026-03-28 22:47:38 +0530

    Lexer main function

--- src/lexer/lexer.js
+++ src/lexer/lexer.js
@@ -53,3 +53,111 @@ var MULTI_CHAR_OPS = [
   '+=', '-=', '*=', '/=',
   '->'
 ];
+
+/* Lexer */
+function lexer(source) {
+  var tokens = [];
+  var i = 0, line = 1, col = 1;
+
+  function peek(offset)   { offset = offset || 0; return source[i + offset]; }
+  function advance() {
+    var ch = source[i++];
+    if (ch === '\n') { line++; col = 1; } else { col++; }
+    return ch;
+  }
+
+  while (i < source.length) {
+    var startLine = line, startCol = col;
+    var ch = peek();
+
+    // Single-line comment
+    if (ch === '/' && peek(1) === '/') {
+      var val = '';
+      while (i < source.length && peek() !== '\n') val += advance();
+      tokens.push({ type: 'COMMENT', value: val.trim(), line: startLine, col: startCol });
+      continue;
+    }
+
+    // Block comment (slash-star to star-slash)
+    if (ch === '/' && peek(1) === '*') {
+      var val = '';
+      advance(); advance();
+      while (i < source.length && !(peek() === '*' && peek(1) === '/')) val += advance();
+      if (i < source.length) { advance(); advance(); }
+      tokens.push({ type: 'COMMENT', value: val.trim(), line: startLine, col: startCol });
+      continue;
+    }
+
+    // Whitespace -- skip
+    if (/\s/.test(ch)) { advance(); continue; }
+
+    // String literal
+    if (ch === '"') {
+      advance();
+      var val = '';
+      while (i < source.length && peek() !== '"') {
+        if (peek() === '\\') { advance(); val += '\\' + advance(); }
+        else val += advance();
+      }
+      if (peek() === '"') advance();
+      tokens.push({ type: 'STRING', value: '"' + val + '"', line: startLine, col: startCol });
+      continue;
+    }
+
+    // Numeric literal
+    if (/[0-9]/.test(ch)) {
+      var val = '';
+      while (i < source.length && /[0-9]/.test(peek())) val += advance();
+      if (peek() === '.' && /[0-9]/.test(peek(1))) {
+        val += advance();
+        while (i < source.length && /[0-9]/.test(peek())) val += advance();
+        tokens.push({ type: 'FLOAT', value: val, line: startLine, col: startCol });
+      } else {
+        tokens.push({ type: 'INTEGER', value: val, line: startLine, col: startCol });
+      }
+      continue;
+    }
+
+    // Identifier / keyword / boolean
+    if (/[a-zA-Z_]/.test(ch)) {
+      var val = '';
+      while (i < source.length && /[a-zA-Z0-9_]/.test(peek())) val += advance();
+      if (val === 'true' || val === 'false')
+        tokens.push({ type: 'BOOL', value: val, line: startLine, col: startCol });
+      else if (KEYWORDS.has(val))
+        tokens.push({ type: 'KEYWORD', value: val, line: startLine, col: startCol });
+      else
+        tokens.push({ type: 'IDENTIFIER', value: val, line: startLine, col: startCol });
+      continue;
+    }
+
+    // Multi-character operators
+    var twoChar = source.substr(i, 2);
+    if (MULTI_CHAR_OPS.indexOf(twoChar) !== -1) {
+      tokens.push({ type: 'OPERATOR', value: twoChar, line: startLine, col: startCol });
+      advance(); advance();
+      continue;
+    }
+
+    // Single-character operator
+    if (OPERATORS.has(ch)) {
+      tokens.push({ type: 'OPERATOR', value: ch, line: startLine, col: startCol });
+      advance();
+      continue;
+    }
+
+    // Punctuation
+    if (PUNCTUATIONS.has(ch)) {
+      tokens.push({ type: 'PUNCTUATION', value: ch, line: startLine, col: startCol });
+      advance();
+      continue;
+    }
+
+    // Unknown character
+    tokens.push({ type: 'UNKNOWN', value: ch, line: startLine, col: startCol });
+    advance();
+  }
+
+  tokens.push({ type: 'EOF', value: '<EOF>', line: line, col: col });
+  return tokens;
+}
