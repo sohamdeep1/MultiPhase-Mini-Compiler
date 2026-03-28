ee45fa29aebcb52ae4f7c6863dec486ddbce8d13
Soham Deep Chauhan <akashdeepchauhan9@gmail.com>
2026-03-28 22:39:26 +0530

    starting work on lexer.js

---
+++ src/lexer/lexer.js
@@ -0,0 +1,11 @@
+/**
+ * src/lexer/lexer.js
+ * Phase 1 -- Lexical Analysis
+ *
+ * Converts raw source code into a flat list of tokens.
+ * Each token: { type, value, line, col }
+ *
+ * Token types:
+ *   KEYWORD | IDENTIFIER | INTEGER | FLOAT | STRING | BOOL
+ *   OPERATOR | PUNCTUATION | COMMENT | EOF | UNKNOWN
+ */
