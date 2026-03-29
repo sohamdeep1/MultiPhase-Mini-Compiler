73748fbbdd3f8a3d708adede7c31144a024b202a
Soham Deep Chauhan <akashdeepchauhan9@gmail.com>
2026-03-30 00:52:01 +0530

    function parser started

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -8,3 +8,20 @@
  *   assign -> or -> and -> equality -> comparison
  *   -> addition -> multiplication -> unary -> postfix -> primary
  */
+
+function parser(tokens) {
+  var toks = tokens.filter(function(t) { return t.type !== 'COMMENT'; });
+  var pos = 0;
+  var parseErrors = [];
+
+  function peek(offset) { return toks[pos + (offset || 0)] || { type: 'EOF', value: '<EOF>' }; }
+  function consume()    { return toks[pos++] || { type: 'EOF', value: '<EOF>' }; }
+
+  function expect(type, val) {
+    var t = peek();
+    if ((type && t.type !== type) || (val && t.value !== val)) {
+      parseErrors.push({ msg: "Expected '" + (val || type) + "' but found '" + t.value + "'", line: t.line });
+      return null;
+    }
+    return consume();
+  }
