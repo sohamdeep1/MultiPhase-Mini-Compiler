a2135cff1bccf81bdb534c34bfb9cc2ce499f96c
Soham Deep Chauhan <akashdeepchauhan9@gmail.com>
2026-03-31 23:05:52 +0530

    started buiding semantic function

--- src/semantic/semantic.js
+++ src/semantic/semantic.js
@@ -9,3 +9,21 @@
  *   4. Duplicate declaration detection
  *   5. Unused variable warnings
  */
+
+function semantic(ast) {
+  var errors      = [];
+  var warnings    = [];
+  var symbolTable = [];
+  var scopes      = [new Map()];
+  var functions   = new Map();
+
+  function currentScope() { return scopes[scopes.length - 1]; }
+  function pushScope()    { scopes.push(new Map()); }
+  function popScope() {
+    currentScope().forEach(function(info, name) {
+      if (!info.used && !BUILTINS.has(name)) {
+        warnings.push({ msg: "Variable '" + name + "' declared but never used", line: info.line });
+      }
+    });
+    scopes.pop();
+  }
